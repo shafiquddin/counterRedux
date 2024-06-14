8500c05c77f8a27ce7bd55e0890762ca3eafebb4
shafiquddin <shafiquddin2k@gmail.com>
2024-06-15 00:14:31 +0530

    redux in class component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,31 +1,45 @@
-import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
+import { Component } from 'react';
+import { connect } from 'react-redux';
 
-const Counter = () => {
-  const dispatch = useDispatch();
-  const counter = useSelector(state => state.counter)
-  const toggleCounterHandler = () => {
+
+class Counter extends Component{
+  toggleCounterHandler = () => {
   };
 
-  const handleIncrement = () => {
-    dispatch({type:'INCR'})
+  handleIncrement = () => {
+    this.props.increment();
   }
 
-  const handleDecrement = () => {
-    dispatch({type:'DECR'})
+   handleDecrement = () => {
+    this.props.decrement();
   }
+  render(){
+    return (
+      <main className={classes.counter}>
+        <h1>Redux Counter</h1>
+        <div className={classes.value}>{this.props.counter}</div>
+        <div>
+          <button onClick={this.handleIncrement.bind(this)}>Increment</button>
+          <button onClick={this.handleDecrement.bind(this)}>Decrement</button>
+        </div>
+        <button onClick={this.toggleCounterHandler.bind(this)}>Toggle Counter</button>
+      </main>
+    );
+  }
+}
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={handleIncrement}>Increment</button>
-        <button onClick={handleDecrement}>Decrement</button>
-      </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
-};
+const mapStateToProps = state => {
+  return{
+    counter : state.counter,
+  }
+}
 
-export default Counter;
+const mapDispatchToProps = dispatch =>{
+  return {
+    increment:()=>dispatch({type:'INCR'}),
+    decrement:()=>dispatch({type:'DECR'})
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps) (Counter);
